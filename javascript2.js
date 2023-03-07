5fdbef4b2657bde946416f9f934ef1186f487e7b
Marco Enzo Solorzano Caiafa <124593557+enzonoire@users.noreply.github.com>
2023-03-07 15:40:26 -0300

    initial commit

---
+++ javascript2.js
@@ -0,0 +1,25 @@
+
+document.getElementById('button').onclick= function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo').innerHTML = "Fecha de nacimiento: 27/03/2002 - Nacionalidad: Argentino - Estado Civil: Soltero - DNI: 43989108 - Domicilio: Salvigny 1460 Nueva Pompeya - Celular: 011-1532709465 - Mail: markienz373@gmail.com";
+}
+
+document.getElementById('button2').onclick= function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo2').innerHTML = "Secundario: Escuela Superior de Comercio N°3 DE 7 Hipolito Vieytes (Perito Mercantil) - Primario: Escuela N°7 DE 10 Manuel J. García";
+}
+
+document.getElementById('button3').onclick= function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo3').innerHTML = "Anfitrión en Cinemark Hoyts (Contrato plazo fijo 04/11 - 23/11)";
+}
+
+document.getElementById('button4').onclick= function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo4').innerHTML = "Word - Excel - HTML - CSS - JavaScript";
+}
+
+document.getElementById('button5').onclick= function () {
+    console.log ("Capturamos el evento click");
+    document.getElementById('demo5').innerHTML = "Inglés intermedio/avanzado";
+}
